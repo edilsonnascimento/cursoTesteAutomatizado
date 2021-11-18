3158e21d82ff76a8e838a6a3358e908630e18e56
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-11-18 15:04:24 -0300

    01 - Importado Projeto do Curso e ajustado para o Itellij.

---
+++ src/test/java/br/edu/enascimento/negocio/GerenciadoraClientesTest.java
@@ -0,0 +1,11 @@
+package br.edu.enascimento.negocio;
+
+import org.junit.jupiter.api.Test;
+
+public class GerenciadoraClientesTest {
+
+    @Test
+    public void testInfra(){
+        System.out.println("teste");
+    }
+}
